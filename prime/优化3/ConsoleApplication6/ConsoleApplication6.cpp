44972fb09d68fd4be0c77292d11171d087565347
20191864122 <2560735192@qq.com>
2019-10-26 23:44:02 +0800

    优化

---
+++ prime/优化3/ConsoleApplication6/ConsoleApplication6.cpp
@@ -0,0 +1,41 @@
+﻿#include <stdio.h>
+#include <time.h>
+#include <math.h>
+int		is_prime(int n);
+int main()
+{
+	int		n, m;
+	int		total = 1;
+	long	t0, t1;
+	m = 100000;
+	t0 = clock();
+	for (n = 3; n <= m; n += 2) {
+	//if(m % 10000 == 0)
+		//printf("%d%%\r", n * 100 / m);
+		if (is_prime(n)) {
+			total++;
+			//printf("\n%-4d %d", n,total);
+		}
+	}
+	t1 = clock();
+printf("\nTotal prime numbers between 2 and %d: %d\nTime used: %10.2f seconds\n", m, total, (t1 - t0) / (float)CLOCKS_PER_SEC);
+	return 0;
+}
+int		is_prime(int n)
+{
+	int		i;
+	int		flag;
+	if (n) {
+		flag = 1;
+		for (i = 3; i <= sqrt(n); i += 2) {
+			if (n % i == 0) {
+				flag = 0;
+				break;
+			}
+		}
+	}
+	else
+		flag = 0;
+	return flag;
+}
+
